baf9646345e6c7d0a4c845dd9f4f848093ae7280
Josh Friese <Friese@Friese.local>
2017-03-30 00:15:44 -0400

    added some styling, working on detailed view for items in categories

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # swapi
+
+##### A long time ago in a galaxy far, far away...
+
+***
+
+#### This application reads data from [a Star Wars API](http://swapi.co "SWapi (dot) co") and allows the user to view information from specific categories.
+
+### Execution
+
+Download or clone this repository and, with your browser of choice ready to go, navigate to the directoy containing index.html and open the file.
+
+
+### Contributors
+
+	#<b>Josh Friese</b> - [phist210](http://phist210.github.com)
--- js/swapi.js
+++ js/swapi.js
@@ -2,8 +2,18 @@ function listCharacter() {
   var people_api = "http://swapi.co/api/people/";
   for (var i = 1; i <= 87; i++) {
   $.ajax({url: people_api + i, success: function(result) {
-    var character = result.name;
-    $('div#list_character').append(character + "</br>");
+    var character = result.name,
+        gender = result.gender,
+        height = result.height,
+        weight = result.mass,
+        dob = result.birth_year,
+        homeworld = result.homeworld;
+        films = result.films,
+        species = result.species,
+        vehicles = result.vehicles,
+        url = result.url[result.url.length -2];
+    $('div#list_character').append("<div id='" + url + "'>" + character + "</br>");
+    console.log(url);
     }});
   }
 }
@@ -12,9 +22,11 @@ function listFilm() {
   var films_api = "http://swapi.co/api/films/";
   for (var i = 1; i <= 6; i++) {
   $.ajax({url: films_api + i, success: function(result) {
-    var film = result.title;
-    $('div#list_film').append(film + "</br>");
-    console.log(film);
+    var film = result.title,
+        episode = result.episode_id,
+        release = result.release_date,
+        url = result.url[result.url.length -2];
+        $('div#list_film').append("<div id='" + url + "'>" + film + "</br>");
     }});
   }
 }
@@ -23,12 +35,19 @@ function listVehicle() {
   var vehicles_api = "http://swapi.co/api/vehicles/";
   for (var i = 1; i <= 39; i++) {
   $.ajax({url: vehicles_api + i, success: function(result) {
-    var vehicle = result.name;
-    $('div#list_vehicle').append(vehicle + "</br>");
+    var vehicle = result.name,
+        crew = result.crew,
+        length = result.length,
+        manufac = result.manufacuterer,
+        pilots = result.pilots,
+        films = result.films;
+        url = result.url[result.url.length -2];
+    $('div#list_vehicle').append("<div id='" + url + "'>" + vehicle + "</br>");
     }});
   }
 }
 
-$('span#characters').click(listCharacter);
-$('span#films').click(listFilm);
-$('span#vehicles').click(listVehicle);
+
+$('span#characters').dblclick(listCharacter);
+$('span#films').dblclick(listFilm);
+$('span#vehicles').dblclick(listVehicle);
